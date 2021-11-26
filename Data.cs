d38b80bd41ea22f0841cef14b6300f88659547e4
Tom Wire <tom.w.wire@gmail.com>
2021-11-26 11:57:58 +0000

    Better data example

---
+++ Data.cs
@@ -0,0 +1,33 @@
+﻿namespace MudBlazorVirtualize
+{
+    public class Data
+    {
+        public string Name { get; set; }
+
+        public int Index { get; }
+
+        public string PartnerName { get; }
+
+        public string Field1 { get; }
+
+        public string Field2 { get; }
+
+        public string Field3 { get; }
+
+        public string Field4 { get; }
+
+        public string Field5 { get; }
+
+        public Data(int index)
+        {
+            Name = $"Name: {index}";
+            Index = index;
+            PartnerName = $"Partner {index}";
+            Field1 = $"Lorum Ipsum {Index}";
+            Field2 = $"Lorum Ipsum {Index}";
+            Field3 = $"Lorum Ipsum {Index}";
+            Field4 = $"Lorum Ipsum {Index}";
+            Field5 = $"Lorum Ipsum {Index}";
+        }
+    }
+}
